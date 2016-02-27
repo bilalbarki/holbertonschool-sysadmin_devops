0a534f15ee695ab4917b03b69994e2e59b63a137
Bilal Ahmad Khan <bilalbarki@gmail.com>
2016-02-28 03:53:09 +0500

    Final Look

--- choice_the_voice_of_your_ai/0-the_human_voice_is_the_organ_of_the_soul.sh
+++ choice_the_voice_of_your_ai/0-the_human_voice_is_the_organ_of_the_soul.sh
@@ -1,1 +1,15 @@
-testing git on linux mint
+#!/bin/bash
+file=$(echo $1 | awk '{print $1}')
+IP=$3;
+case $2 in
+    m)
+        espeak -ven-us+m4 -s100 "$1" --stdout > $file.wav;;
+    f)
+        espeak -ven-us+f4 -s100 "$1" --stdout > $file.wav;;
+    x)
+	espeak -s 40 -v en+m5 -p 70 "$1" --stdout > $file.wav;;
+esac
+
+scp $file.wav admin@$IP:/var/www/html/$file.wav
+echo "Listen to the message on http://$IP/$file.wav"
+rm $file.wav
