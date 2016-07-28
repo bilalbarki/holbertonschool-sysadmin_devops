5f833a2f6305f015cd81fa49c813b3793243ad47
bilalbarki <bilalbarki@gmail.com>
2016-07-28 09:12:39 +0500

    first look

---
+++ chef_is_fun/cookbooks/holberton-wrapper/metadata.rb
@@ -0,0 +1,12 @@
+name             'holberton-wrapper'
+maintainer       'Bilal Khan'
+maintainer_email '40@holbertonschool.com'
+license          'All rights reserved'
+description      'Installs/Configures holberton-wrapper'
+long_description 'Installs/Configures holberton-wrapper'
+version          '0.1.0'
+
+depends "apt"
+depends "ntp"
+depends "openssh"
+depends "user"
