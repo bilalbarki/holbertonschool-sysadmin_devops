c16844b31becdafe4fc2f44d71bd0019a83df63b
bilalbarki <bilalbarki@gmail.com>
2016-08-10 03:51:18 +0500

    first look

---
+++ on_call_1/0-command-line-history.sh
@@ -0,0 +1,110 @@
+    1  rm ~/.bash_history 
+    2  sudo su
+    3  man nginx
+    4  service nginx status
+    5  sudo service nginx start
+    6  service nginx status
+    7  sudo service nginx start
+    8  service nginx status
+    9  cat /var/log/syslog 
+   10  crontab -e
+   11  sudo crontab -e
+   12  sudo bash
+   13  cat /var/log/syslog 
+   14  sudo /etc/init.d/nginx start
+   15  nginx -t
+   16  sudo nginx -t
+   17  sudo iptables -L
+   18  sudo iptables -F
+   19  sudo iptables -L
+   20  sudo service apache2 stop
+   21  sudo service /etc/init.d/apache2 stop
+   22  sudo /etc/init.d/apache2 stop
+   23  sudo service nginx start
+   24  sudo service nginx status
+   25  nginx -t
+   26  sudo nginx -t
+   27  sudo /etc/init.d/nginx start
+   28  sudo service nginx status
+   29  cat /var/log/syslog 
+   30  lsof -i :80
+   31  lsof -i :8080
+   32  lsof
+   33  sudo netstat -peant | grep ":80 "
+   34  sudo netstat -peant | grep ":8080 "
+   35  systemctl status nginx.service
+   36  nginx -t -c /etc/nginx/nginx.conf
+   37  sudo nginx -t -c /etc/nginx/nginx.conf
+   38  cd /etc/nginx/sites-enabled/
+   39  ls
+   40  cd def
+   41  sudo emacs default 
+   42  sudo service nginx start
+   43  sudo service nginx status
+   44  cd /usr/share/nginx/www
+   45  sudo service nginx status
+   46  ls /usr/share/nginx/html
+   47  cat /usr/share/nginx/html/index.html 
+   48  cd /etc/nginx/sites-enabled/
+   49  sudo emacs default 
+   50  sudo service nginx restart
+   51  sudo service nginx status
+   52  sudo service nginx start
+   53  sudo service nginx status
+   54  sudo emacs default 
+   55  sudo service nginx start
+   56  sudo service nginx status
+   57  sudo service nginx start
+   58  sudo service nginx status
+   59  sudo emacs default 
+   60  sudo nginx -t
+   61  ls
+   62  rm \#default# 
+   63  sudo rm \#default# 
+   64  nginx -t
+   65  sudo nginx -t
+   66  sudo service nginx start
+   67  sudo service nginx status
+   68  curl 127.0.0.1
+   69  curl 127.0.0.1:80
+   70  sudo netstat -peant | grep ":80 "
+   71  sudo netstat -peant | grep "nginx"
+   72  sudo netstat -peant
+   73  curl http://localhost/
+   74  lsof -i :80
+   75  lsof -i
+   76  lsof
+   77  netstat -tulnp | grep 80
+   78  netstat -a
+   79  netstat -a | grep nginx
+   80  sudo service nginx status
+   81  sudo service nginx start
+   82  sudo service nginx status
+   83  netstat -a | grep nginx
+   84  netstat -tulnp | grep 80
+   85  `curl http://localhost
+:
+:
+"
+   86  `curl http://localhost"
+   87  curl http://localhost"
+   88  curl "http://localhost"
+   89  sudo service nginx status
+   90  sudo service nginx start
+   91  curl "http://localhost"
+   92  ls /etc
+   93  cat /var/log/syslog 
+   94  sudo service nginx start
+   95  sudo service nginx status
+   96  cat /var/log/syslog 
+   97  ps
+   98  sudo ps
+   99  top
+  100  sudo service /etc/init.d/cron status
+  101  sudo /etc/init.d/cron status
+  102  sudo /etc/init.d/cron stop
+  103  stop cron
+  104  sudo service cron stop
+  105  sudo service cron status
+  106  sudo service nginx start
+  107  history
