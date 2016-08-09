83e43f97f78740b341f19d65cfc9aee8acc32c9e
bilalbarki <bilalbarki@gmail.com>
2016-08-09 12:45:13 +0500

    added optionals

--- infrastructure_as_code_with_aws_ec2/0-manage_ec2_instances.rb
+++ infrastructure_as_code_with_aws_ec2/0-manage_ec2_instances.rb
@@ -5,18 +5,24 @@ require 'optparse'
 require 'yaml'
 
 options = {}
+help = ""
 
 OptionParser.new do |opts|
 	opts.banner = "Usage: 0-manage_ec2_instances.rb [options]"
 
 	# main actions
-	opts.on('-a','--action [ACTION]', [:launch, :stop, :start, :terminate], "Select action from launch, stop, start and terminate") do |t|
+	opts.on('-a','--action [ACTION]', [:launch, :stop, :start, :terminate, :status, :change_name], "Select action from launch, stop, start, terminate, status, change_name") do |t|
 		options[:action] = t
 	end
 
 	# for server id
-	opts.on('-i', '--server_id [SERVER_ID]', 'Provide the server id') do |s|
-		options[:server_id] = s
+	opts.on('-i', '--instance_id [INSTANCE_ID]', 'Provide the server id') do |s|
+		options[:instance_id] = s
+	end
+
+	#to input name
+	opts.on('-n', '--name [name]', 'Provide the name for changing it') do |n|
+		options[:n] = n
 	end
 
 	# for verbose
@@ -30,11 +36,14 @@ OptionParser.new do |opts|
 		exit
 	end
 
+	help = opts
+
 end.parse!
 
 # Check if an action was passed by the user
 if options[:action].nil? then
-	raise OptionParser::MissingArgument, "Error: Usage -a [launch, stop, start, terminate] or -- action [launch, stop, start, terminate]" 
+	puts help
+	exit
 end
 
 # load yaml file
@@ -48,7 +57,7 @@ client = Aws::EC2::Client.new({
 
 ################ LAUNCH ################
 if options[:action] == :launch then
-	ec2 = Aws::EC2::Resource.new(client: client)	
+	ec2 = Aws::EC2::Resource.new(client: client)
 	instance = ec2.create_instances({
   		image_id: config["image_id"],
   		min_count: 1,
@@ -69,30 +78,48 @@ if options[:action] == :launch then
 
 ################# STOP #################
 elsif options[:action] == :stop then
-	raise OptionParser::MissingArgument, "Error: SERVER_ID is required" if options[:server_id].nil?
+	raise OptionParser::MissingArgument, "Error: INSTANCE_ID is required" if options[:instance_id].nil?
 	client.stop_instances({
 		  dry_run: false,
-		  instance_ids: [options[:server_id]],
+		  instance_ids: [options[:instance_id]],
 		  force: false,
 		})
 
 ################ START ################
 elsif options[:action] == :start then
-	raise OptionParser::MissingArgument, "Error: SERVER_ID is required" if options[:server_id].nil?
+	raise OptionParser::MissingArgument, "Error: INSTANCE_ID is required" if options[:instance_id].nil?
 	out = client.start_instances({
-		  instance_ids: [options[:server_id]],
+		  instance_ids: [options[:instance_id]],
 		  dry_run: false,
 		})
-	out = client.wait_until(:instance_running, instance_ids:[options[:server_id]])
+	out = client.wait_until(:instance_running, instance_ids:[options[:instance_id]])
 	dns_name = out.reservations[0].instances[0].public_dns_name
 	puts dns_name
 
 
 ############## TERMINATE ##############
 elsif options[:action] == :terminate then
-	raise OptionParser::MissingArgument, "Error: SERVER_ID is required" if options[:server_id].nil?
+	raise OptionParser::MissingArgument, "Error: INSTANCE_ID is required" if options[:instance_id].nil?
 	client.terminate_instances({
 		  dry_run: false,
-		  instance_ids: [options[:server_id]],
+		  instance_ids: [options[:instance_id]],
 	})
+
+elsif options[:action] == :status then
+	raise OptionParser::MissingArgument, "Error: INSTANCE_ID is required" if options[:instance_id].nil?
+	ec2 = Aws::EC2::Resource.new(client: client)
+	i = ec2.instance(options[:instance_id])
+	if i.exists?
+		puts i.state.name
+	end
+
+elsif options[:action] == :change_name then
+	raise OptionParser::MissingArgument, "Error: INSTANCE_ID is required" if options[:instance_id].nil?
+	raise OptionParser::MissingArgument, "Error: NAME is required" if options[:n].nil?
+
+	ec2 = Aws::EC2::Resource.new(client: client)
+	i = ec2.instance(options[:instance_id])
+	if i.exists?
+		i.create_tags({ tags: [{ key: 'Name', value: options[:n] }]})
+	end
 end
